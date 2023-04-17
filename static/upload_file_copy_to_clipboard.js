1b0727fc699087101391e2f9c16c175677970d33
Maxim Shutov <maxius.2302@gmail.com>
2023-04-17 21:05:43 +0400

    Copy button fix

--- static/upload_file_copy_to_clipboard.js
+++ static/upload_file_copy_to_clipboard.js
@@ -4,9 +4,5 @@ function copyToClipboard(TextElementID) {
     copyText.select();
     copyText.setSelectionRange(0, 99999); // For mobile devices
 
-    try {
-        navigator.clipboard.writeText(copyText.value);
-    } catch (e) {
-        console.log("Probably there's no SSL certificates.");
-    }
+    document.execCommand("copy");
 }
